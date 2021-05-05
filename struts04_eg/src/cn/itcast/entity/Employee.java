7177ecb3abef201a10526882c5833857c07cbe1c
Charlotte <hunandy14@gmail.com>
2021-05-05 15:20:07 +0900

    寫完測試

--- struts04_eg/src/cn/itcast/entity/Employee.java
+++ struts04_eg/src/cn/itcast/entity/Employee.java
@@ -1,5 +1,7 @@
 package cn.itcast.entity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Employee {
@@ -8,6 +10,16 @@ public class Employee {
 	private String empName;// VARCHAR(20),
 	private Date workDate;// DATE -- 入职时间
 	
+	
+	public Employee() {
+		super();
+	}
+	public Employee(int id, String empName, String workDate) {
+		this.id = id;
+		this.empName = empName;
+		this.workDate = coverToDate(workDate);
+	}
+
 	
 	public int getId() {
 		return id;
@@ -27,5 +39,27 @@ public class Employee {
 	public void setWorkDate(Date workDate) {
 		this.workDate = workDate;
 	}
+	@Override
+	public String toString() {
+		String date_str=null;
+		if(workDate!=null){			
+			date_str = new SimpleDateFormat("yyyy-MM-dd").format(workDate);
+		}
+		return "Employee [id=" + id + ", empName=" + empName + ", workDate="
+				+ workDate + "]";
+	}
 	
+	
+	// 字串轉換到日期
+	private Date coverToDate(String Date) {
+		Date date = null;
+        SimpleDateFormat df = new  SimpleDateFormat("yyyy-MM-dd");
+        try {
+			date = df.parse(Date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return date;
+	}
 }
