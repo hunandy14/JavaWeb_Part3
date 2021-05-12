96c890d1b18861afaac45bf570556d91a79bca40
Charlotte <hunandy14@gmail.com>
2021-05-13 03:57:33 +0900

    day36 part2 init

---
+++ spring-struts/src/cn/itcast/action/UserAction.java
@@ -0,0 +1,28 @@
+package cn.itcast.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import cn.itcast.service.UserService;
+
+public class UserAction extends ActionSupport {
+
+	// springIOC容器注入
+	private UserService userService;
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+
+	public String execute() {
+		userService.save();
+		return SUCCESS;
+	}
+}
+
+
+
+
+
+
+
+
