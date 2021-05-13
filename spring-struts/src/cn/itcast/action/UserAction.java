0230db2c99f5253454f6bf0c5483538e7ac456df
Charlotte <hunandy14@gmail.com>
2021-05-14 05:12:23 +0900

    先讓線下可以跑測試中

--- spring-struts/src/cn/itcast/action/UserAction.java
+++ spring-struts/src/cn/itcast/action/UserAction.java
@@ -5,14 +5,16 @@ import com.opensymphony.xwork2.ActionSupport;
 import cn.itcast.service.UserService;
 
 public class UserAction extends ActionSupport {
-
+	private static final long serialVersionUID = 1L;
+	
 	// springIOC容器注入
 	private UserService userService;
+	
+	
 	public void setUserService(UserService userService) {
 		this.userService = userService;
 	}
 	
-
 	public String execute() {
 		userService.save();
 		return SUCCESS;
