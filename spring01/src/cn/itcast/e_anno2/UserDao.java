f3e0f3f37265b617975753711bc3a11cc7078636
Charlotte <hunandy14@gmail.com>
2021-05-13 20:59:12 +0900

    搞定註解的spring

--- spring01/src/cn/itcast/e_anno2/UserDao.java
+++ spring01/src/cn/itcast/e_anno2/UserDao.java
@@ -1,10 +1,13 @@
 package cn.itcast.e_anno2;
 
+import org.springframework.stereotype.Repository;
+
 // 把当前对象加入ioc容器
 //@Component("userDao")   //  相当于bean.xml 【<bean id=userDao class=".." />】
 
 //@Component  // 加入ioc容器的UserDao对象的引用名称， 默认与类名一样， 且第一个字母小写
 
+// 可以省略是因為配置文件裡面有新增了
 //@Repository   // 在持久层可以选择用这个注解
 public class UserDao {
 	
--- spring01/src/cn/itcast/e_anno2/UserService.java
+++ spring01/src/cn/itcast/e_anno2/UserService.java
@@ -11,9 +11,9 @@ import org.springframework.stereotype.Service;
 @Service   // 表示业务逻辑层的组件
 public class UserService {
 	
-//	@Resource					//  根据类型查找 【在容器中要确保该类型只有一个变量】
+	@Resource					//  根据类型查找 【在容器中要确保该类型只有一个变量】
 	
-	@Resource(name = "userDao")  // 根据名称查找
+//	@Resource(name = "userDao")  // 根据名称查找
 	private UserDao userDao;  // 去容器中招UserDao类型的变量，找到后就赋值
 
 	public void save() {
