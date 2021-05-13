5f59b40ef668f7ac333e83352a4362488f8de48e
Charlotte <hunandy14@gmail.com>
2021-05-14 05:15:15 +0900

    struts2的部分做好了，現在把spring加入

---
+++ struts_test/src/cn/itcast/test/Teser.java
@@ -0,0 +1,42 @@
+package cn.itcast.test;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.itcast.action.UserAction;
+import cn.itcast.dao.UserDao;
+import cn.itcast.service.UserService;
+
+public class Teser {
+	private static ApplicationContext ac_dao;
+	private static ApplicationContext ac_srv;
+	private static ApplicationContext ac_act;
+	
+	private static UserDao ud = new UserDao();
+	private static UserService us = new UserService();
+	
+	@Test
+	public void service(){
+		us.setUserDao(ud);
+		us.save();
+	}
+
+	@Test
+	public void spring_dao(){
+		ac_dao = new ClassPathXmlApplicationContext("1bean-dao.xml");
+		ud = (UserDao) ac_dao.getBean("userDao");
+		ud.save();
+	}
+	@Test
+	public void spring_service(){
+		ac_srv = new ClassPathXmlApplicationContext("1bean-service.xml");
+	}
+	
+	@Test
+	public void spring_action(){
+		 ac_act = new ClassPathXmlApplicationContext("bean-action.xml");
+		UserAction ua = (UserAction) ac_act.getBean("userAction");
+	}
+	
+}
