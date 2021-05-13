15b2da76831b9a3a12adc79a941bdc4e92a13c02
Charlotte <hunandy14@gmail.com>
2021-05-14 08:04:41 +0900

    初始化 day37

---
+++ spring02/src/cn/itcast/d_myaop/IUserDao.java
@@ -0,0 +1,8 @@
+package cn.itcast.d_myaop;
+
+// 接口
+public interface IUserDao {
+
+	void save();
+	
+}
---
+++ spring02/src/cn/itcast/f_aop_xml/IUserDao.java
@@ -0,0 +1,6 @@
+package cn.itcast.f_aop_xml;
+
+// 接口
+public interface IUserDao {
+	void save();
+}
