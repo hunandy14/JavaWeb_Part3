0c14d3a80b845a39e003e25389d7e4b42c33ff76
Charlotte <hunandy14@gmail.com>
2021-05-16 00:52:24 +0900

    初始化ssh

---
+++ ssh/src/cn/itcast/service/EmployeeService.java
@@ -0,0 +1,26 @@
+package cn.itcast.service;
+
+import java.io.Serializable;
+
+import org.hibernate.SessionFactory;
+
+import cn.itcast.dao.EmployeeDao;
+import cn.itcast.entity.Employee;
+
+public class EmployeeService {
+	
+	// IOC注入
+	private EmployeeDao employeeDao;
+	public void setEmployeeDao(EmployeeDao employeeDao) {
+		this.employeeDao = employeeDao;
+	}
+
+	/**
+	 * 查询
+	 * @param emp
+	 */
+	public Employee findById(Serializable id) {
+		Employee emp = employeeDao.findById(id);
+		return emp;
+	}
+}
