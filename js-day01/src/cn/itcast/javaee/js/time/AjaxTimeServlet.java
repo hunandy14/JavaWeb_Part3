905865469a6c0d97f44d78c5a106269e0ac41682
Charlotte <hunandy14@gmail.com>
2021-05-24 02:49:08 +0900

    新增 day40

---
+++ js-day01/src/cn/itcast/javaee/js/time/AjaxTimeServlet.java
@@ -0,0 +1,33 @@
+package cn.itcast.javaee.js.time;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 无需刷新整个Web页面显示服务器响应的当前时间
+ * @author AdminTC
+ */
+public class AjaxTimeServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String nowStr = sdf.format(new Date());
+		
+		//以流的方式将结果响应到AJAX异步对象中
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.write(nowStr);
+		pw.flush();
+		pw.close();
+	}
+}
+
+
+
+
+
---
+++ js-day01/src/cn/itcast/javaee/js/user/UserServlet.java
@@ -0,0 +1,53 @@
+package cn.itcast.javaee.js.user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 基于HTML，以GET或POST方式，检查注册用户名是否在数据库中已存在 
+ * @author AdminTC
+ */
+public class UserServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		String username = request.getParameter("username");
+		byte[] buf = username.getBytes("ISO8859-1");
+		username = new String(buf,"UTF-8");
+		System.out.println("username=" + username);
+		String tip = "<font color='green'>可以注册</font>";
+		if("杰克".equals(username)){
+			tip = "<font color='red'>该用户已存在</font>";
+		}
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.write(tip);
+		pw.flush();
+		pw.close();
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String username = request.getParameter("username");
+		System.out.println("username=" + username);
+		
+		String tip = "images/MsgSent.gif";
+		if("杰克".equals(username)){
+			tip = "images/MsgError.gif";
+		}
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.write(tip);
+		pw.flush();
+		pw.close();
+	}
+}
+
+
+
+
+
+
+
